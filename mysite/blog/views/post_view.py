770b91b6bcaf984875bc4d34c04328dc4976ce4b
e_sibuya@outlook.com <*e_sibuya@outlook.com*>
2023-07-17 17:59:07 -0300

    criando templates

--- mysite/blog/views/post_view.py
+++ mysite/blog/views/post_view.py
@@ -1,7 +1,13 @@
-from django.http import HttpResponse
 from django.views import generic
 
-class PostView(generic.View):
-    
-    def get(self, request, *args, **kwargs):
-        return HttpResponse('Tarefa ebac')
+from blog.models import Post
+
+# Aqui estamos renderizando o index.html e usando o listview para listar e filtrar os posts com status 1, dentro do index.html
+class PostView(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by("-created_on")
+    template_name = "index.html"
+
+# Aqui estamos renderizando o post_detail.html
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = "post_detail.html"
